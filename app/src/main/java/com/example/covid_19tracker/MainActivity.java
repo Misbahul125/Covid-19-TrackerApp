42579f6bcb53b69c50a247d2b66ab969b689e3e3
Misbahul Haque <misbahulhaque2001@gmail.com>
2021-09-01 01:50:33 +0530

    Launcher Activity and API call added

--- app/src/main/java/com/example/covid_19tracker/MainActivity.java
+++ app/src/main/java/com/example/covid_19tracker/MainActivity.java
@@ -2,8 +2,26 @@ package com.example.covid_19tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.covid_19tracker.api.ApiUtilities;
+import com.example.covid_19tracker.api.CountryModel;
+
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
+
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +29,12 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView textViewTotalConfirmed , textViewTodayConfirmed , textViewTotalActive , textViewTotalRecovered , textViewTodayRecovered ,
             textViewTotalDeath , textViewTodayDeath , textViewTotalTest;
+
+    private long lastUpdated;
+    private int totalConfirmed , todayConfirmed , totalActive , totalRecovered , todayRecovered , totalDeath , todayDeath , totalTest;
+
+    private PieChart pieChart;
+    private List<CountryModel> countryModelList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +51,67 @@ public class MainActivity extends AppCompatActivity {
         textViewTotalDeath = findViewById(R.id.total_death);
         textViewTodayDeath = findViewById(R.id.today_death);
         textViewTotalTest = findViewById(R.id.total_tests);
+        pieChart = findViewById(R.id.pie_chart);
+
+        countryModelList = new ArrayList<>();
+
+        ApiUtilities.getApiInterface()
+                .getCountryData()
+                .enqueue(new Callback<List<CountryModel>>() {
+                    @Override
+                    public void onResponse(Call<List<CountryModel>> call, Response<List<CountryModel>> response) {
+                        if (response.body() != null) {
+                            countryModelList.addAll(response.body());
+
+                            for (int i = 0 ; i<countryModelList.size() ; i++) {
+
+                                if (countryModelList.get(i).getCountry().equals("India")) {
+
+                                    textViewCountryName.setText(countryModelList.get(i).getCountry());
+
+                                    lastUpdated = Long.parseLong(countryModelList.get(i).getUpdated());
+                                    totalConfirmed = Integer.parseInt(countryModelList.get(i).getCases());
+                                    todayConfirmed = Integer.parseInt(countryModelList.get(i).getTodayCases());
+                                    totalActive = Integer.parseInt(countryModelList.get(i).getActive());
+                                    totalRecovered = Integer.parseInt(countryModelList.get(i).getRecovered());
+                                    todayRecovered = Integer.parseInt(countryModelList.get(i).getTodayRecovered());
+                                    totalDeath = Integer.parseInt(countryModelList.get(i).getDeaths());
+                                    todayDeath = Integer.parseInt(countryModelList.get(i).getTodayDeaths());
+                                    totalTest = Integer.parseInt(countryModelList.get(i).getTests());
+
+                                    DateFormat dateFormat = DateFormat.getDateTimeInstance();
+                                    textViewCountryLastUpdated.setText(("Last updated at  :  ")+dateFormat.format(lastUpdated));
+
+                                    NumberFormat numberFormat = NumberFormat.getInstance();
+                                    textViewTotalConfirmed.setText(numberFormat.format(totalConfirmed));
+                                    textViewTodayConfirmed.setText(("+")+numberFormat.format(todayConfirmed));
+                                    textViewTotalActive.setText(numberFormat.format(totalActive));
+                                    textViewTotalRecovered.setText(numberFormat.format(totalRecovered));
+                                    textViewTodayRecovered.setText(("+")+numberFormat.format(todayRecovered));
+                                    textViewTotalDeath.setText(numberFormat.format(totalDeath));
+                                    textViewTodayDeath.setText(("+")+numberFormat.format(todayDeath));
+                                    textViewTotalTest.setText(numberFormat.format(totalTest));
+
+                                    pieChart.addPieSlice(new PieModel("Confirmed", totalConfirmed, getResources().getColor(R.color.yellow)));
+                                    pieChart.addPieSlice(new PieModel("Active", totalActive, getResources().getColor(R.color.blue_pie)));
+                                    pieChart.addPieSlice(new PieModel("Recovered", totalRecovered, getResources().getColor(R.color.green_pie)));
+                                    pieChart.addPieSlice(new PieModel("Deaths", totalDeath, getResources().getColor(R.color.red_pie)));
+
+                                    pieChart.startAnimation();
+                                }
+                            }
+
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<CountryModel>> call, Throwable t) {
+                        Log.d("ERROR" , t.getMessage());
+                        Toast.makeText(MainActivity.this, "Error in fetching data. Please try again after sometime.",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
 
     }
 }
--- app/src/main/java/com/example/covid_19tracker/api/ApiUtilities.java
+++ app/src/main/java/com/example/covid_19tracker/api/ApiUtilities.java
@@ -1,4 +1,21 @@
 package com.example.covid_19tracker.api;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ApiUtilities {
+
+    private static Retrofit retrofit = null;
+
+    public static RestAPI getApiInterface() {
+
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(RestAPI.BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit.create(RestAPI.class);
+    }
 }
